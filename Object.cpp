773ac238262d59d597ff9ff373013d9525047da8
imyoungkyuu <sun4son@nate.com>
2017-09-20 11:12:50 +0900

    Add files via upload

---
+++ Object.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Object.h"
+#include "Renderer.h"
+
+Object::Object()
+{
+}
+
+
+Object::~Object()
+{
+}
+
+void Object::ObInitialized()
+{
+
+}
---
+++ Object.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Renderer.h"
+
+
+class Object
+{
+	void ObInitialized();
+
+public:
+	Object();
+	~Object();
+
+};
+
